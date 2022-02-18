59acc1934de49fa855dab5064a09290e2983e6ab
Kevin Luzbetak <luzbetak5739@gmail.com>
2022-02-18 10:13:58 -0800

    Sum of Two

---
+++ My_Integer/puzzle-sum-of-two.py
@@ -0,0 +1,16 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, v in enumerate(nums):
+        remaining = target - v
+        if remaining in seen:
+            return [seen[remaining], i]
+        seen[v] = i
+    return []
+
+
+# -------------------------------------------------- #
+# O(n2)
+# Result: [1, 4] = 3 + 2 = 5
+# -------------------------------------------------- #
+result = two_sum([8, 3, 6, 1, 2, 4], 5)
+print(result)
